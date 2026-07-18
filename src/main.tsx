e4d179a86ee67adfdd4c23c6c86cbe7349d7eaef
Carlos <rauda.carlos6432@gmail.com>
2026-07-18 12:01:46 -0600

    Refactor application to use Tailwind CSS and remove custom styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("No se encontró el elemento con id 'root' en el HTML.");
+}
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 )
